6dc2cf0d7119c1cd8d9e89518277cef8da51161a
Loak <loakyong@gmail.com>
2024-06-03 02:47:19 +0800

    update

--- C/2024060200/04/linklist_all_v2.c
+++ C/2024060200/04/linklist_all_v2.c
@@ -12,12 +12,12 @@ typedef struct LNode {
 
 int InitList(LinkList *L);                          //初始化单链表（带头结点）
 int ListInsert(LinkList *L, int i, ElemType e);     //在表L第i个位置插入元素e（头插法）
-int InsertNextNode(LNode *p, ElemType e);           //（表存在）尾插法，插入在p节点之后
-int InsertPriorNode(LNode *p, ElemType e);          //（表存在）前插法，插入在p节点之前（仅交换前后值）
-int ListDelete(LinkList *L, int i,int *e);          //在表L第i个位置删除节点，并用e的值返回
-int DeleteNode(LNode *p);                           //（表存在）删除指定节点p
+int InsertNextNode(LNode *p, ElemType e);           //（表存在）尾插法，插入在p结点之后
+int InsertPriorNode(LNode *p, ElemType e);          //（表存在）前插法，插入在p结点之前（仅交换前后值）
+int ListDelete(LinkList *L, int i,int *e);          //在表L第i个位置删除结点，并用e的值返回
+int DeleteNode(LNode *p);                           //（表存在）删除指定结点p
 LNode *GetElem(LinkList L, int i);                  //按位查找，返回第i个元素
-LNode *LocateElem(LinkList L, ElemType e);          //按值查找，找到数据域 == e的节点（ElemType为int类型）
+LNode *LocateElem(LinkList L, ElemType e);          //按值查找，找到数据域 == e的结点（ElemType为int类型）
 int Length(LinkList L);                             //求表长
 void Print(LinkList L);                             //打印链表元素
 
@@ -90,21 +90,21 @@ int ListDelete(LinkList *L, int i,int *e)
         return FALSE;
     if (p->next == NULL)
         return FALSE;
-    LNode *del = p->next;       //del指向被删除的节点
+    LNode *del = p->next;       //del指向被删除的结点
     (*e) = del->data;           //用e返回元素值
-    p->next = del->next;        //将*del节点从链断开
-    free(del);                  //释放节点存储空间
+    p->next = del->next;        //将*del结点从链断开
+    free(del);                  //释放结点存储空间
     return TRUE;
 }
 
-int DeleteNode(LNode *p)        //注：若p为最后节点，会出现空指针错误
+int DeleteNode(LNode *p)        //注：若p为最后结点，会出现空指针错误
 {
     if (p == NULL)
         return FALSE;
-    LNode *del = p->next;       //del指向*p的后继节点
-    p->data = p->next->data;    //和后继节点交换数据域
-    p->next = del->next;        //将*del节点从链断开
-    free(del);                  //释放后继节点存储空间
+    LNode *del = p->next;       //del指向*p的后继结点
+    p->data = p->next->data;    //和后继结点交换数据域
+    p->next = del->next;        //将*del结点从链断开
+    free(del);                  //释放后继结点存储空间
     return TRUE;
 }
 
@@ -117,7 +117,7 @@ LNode *GetElem(LinkList L, int i)
     LNode *p;
     int j =0;                       ////当前p指向的是第几个结点
     p = L;                          //L指向头结点
-    while (p != NULL && j<i)        //找到第i个节点
+    while (p != NULL && j<i)        //找到第i个结点
     {
         p = p->next;
         j++;
@@ -127,12 +127,12 @@ LNode *GetElem(LinkList L, int i)
 
 LNode *LocateElem(LinkList L, ElemType e)
 {
-    LNode *p = L->next;                             //从第一个节点开始查找
+    LNode *p = L->next;                             //从第一个结点开始查找
     while (p != NULL && p->data != e)
     {
         p = p->next;
     }
-    return p;                                       //找到后返回该节点指针，否则返回NULL
+    return p;                                       //找到后返回该结点指针，否则返回NULL
 }
 
 int Length(LinkList L)
